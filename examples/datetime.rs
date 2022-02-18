7e31e741e0a2ff635661d0cce74a0bb7aa368618
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2022-02-18 17:45:52 +0100

    Update example

--- examples/datetime.rs
+++ examples/datetime.rs
@@ -10,17 +10,19 @@ fn main() -> Result<(), TzError> {
 
     // Get UTC time zone
     let time_zone_utc = TimeZone::utc();
-    assert_eq!(time_zone_utc.find_local_time_type(unix_time)?.ut_offset(), 0);
+    println!("{:?}", time_zone_utc.find_local_time_type(unix_time)?);
 
     // Get fixed time zone at GMT-1
     let time_zone_fixed = TimeZone::fixed(-3600);
-    assert_eq!(time_zone_fixed.find_local_time_type(unix_time)?.ut_offset(), -3600);
+    println!("{:?}", time_zone_fixed.find_local_time_type(unix_time)?.ut_offset());
 
     // Get local time zone
     let time_zone_local = TimeZone::local()?;
+    println!("{:?}", time_zone_local.find_local_time_type(unix_time)?.ut_offset());
 
     // Get the current local time type
-    let _current_local_time_type = time_zone_local.find_current_local_time_type()?;
+    let current_local_time_type = time_zone_local.find_current_local_time_type()?;
+    println!("{:?}", current_local_time_type);
 
     // Get time zone from a TZ string:
     // From an absolute file
@@ -40,16 +42,7 @@ fn main() -> Result<(), TzError> {
     // 2000-01-01T00:00:00Z
     let utc_date_time = UtcDateTime::new(2000, 0, 1, 0, 0, 0)?;
     let date_time = utc_date_time.to_date_time();
-    assert_eq!(date_time.second(), 0);
-    assert_eq!(date_time.minute(), 0);
-    assert_eq!(date_time.hour(), 0);
-    assert_eq!(date_time.month_day(), 1);
-    assert_eq!(date_time.month(), 0);
-    assert_eq!(date_time.year(), 100);
-    assert_eq!(date_time.full_year(), 2000);
-    assert_eq!(date_time.week_day(), 6);
-    assert_eq!(date_time.year_day(), 0);
-    assert_eq!(date_time.local_time_type().ut_offset(), 0);
+    println!("{:#?}", date_time);
 
     // Create a date time from a time zone and an UTC date time
     let time_zone_fixed = TimeZone::fixed(-3600);
@@ -57,38 +50,20 @@ fn main() -> Result<(), TzError> {
     // 2000-01-01T00:00:00Z
     let utc_date_time = UtcDateTime::new(2000, 0, 1, 0, 0, 0)?;
     let date_time = DateTime::from_utc_date_time(&time_zone_fixed, utc_date_time)?;
-    assert_eq!(date_time.second(), 0);
-    assert_eq!(date_time.minute(), 0);
-    assert_eq!(date_time.hour(), 23);
-    assert_eq!(date_time.month_day(), 31);
-    assert_eq!(date_time.month(), 11);
-    assert_eq!(date_time.year(), 99);
-    assert_eq!(date_time.full_year(), 1999);
-    assert_eq!(date_time.week_day(), 5);
-    assert_eq!(date_time.year_day(), 364);
-    assert_eq!(date_time.local_time_type().ut_offset(), -3600);
+    println!("{:#?}", date_time);
 
     // Create a date time from a time zone and an unix time
     // 2000-01-01T00:00:00Z
     let date_time = DateTime::from_unix_time(&time_zone_fixed, 946684800)?;
-    assert_eq!(date_time.second(), 0);
-    assert_eq!(date_time.minute(), 0);
-    assert_eq!(date_time.hour(), 23);
-    assert_eq!(date_time.month_day(), 31);
-    assert_eq!(date_time.month(), 11);
-    assert_eq!(date_time.year(), 99);
-    assert_eq!(date_time.full_year(), 1999);
-    assert_eq!(date_time.week_day(), 5);
-    assert_eq!(date_time.year_day(), 364);
-    assert_eq!(date_time.local_time_type().ut_offset(), -3600);
+    println!("{:#?}", date_time);
 
     // Get the corresponding UTC unix time
-    let unix_time = date_time.unix_time();
-    assert_eq!(unix_time, 946684800);
+    println!("{}", date_time.unix_time());
 
     // Get the current date time at the local time zone
     let time_zone_local = TimeZone::local()?;
-    let _date_time = DateTime::now(&time_zone_local)?;
+    let date_time = DateTime::now(&time_zone_local)?;
+    println!("{:#?}", date_time);
 
     Ok(())
 }
