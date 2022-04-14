62db843022023876f798e76fbb38f1c02e113096
Ryan Lopopolo <rjl@hyperbo.la>
2022-04-13 20:22:02 -0700

    Make `const_fn` an optional dependency
    The docs for the latest `const_fn` have a snippet on how to use the
    attribute macro as an optional dependency [0].
    
    [0] https://docs.rs/const_fn/0.4.9/const_fn/#use-this-crate-as-an-optional-dependency
    
    These two attributes have the same effect:
    
        #[const_fn(feature = "const")]
        pub const fn whatever() {}
    
        #[cfg_attr(feature = "const", const_fn::const_fn)]
        pub fn whatever() {}
    
    But the `cfg_attr` version only requires `const_fn` crate to be a
    dependency when the `const` feature is enabled.
    
    This enables building `tz-rs` without any dependencies and without any
    proc macros.
    
    This was mostly an automated refactor with `sed`. I took a peek at the
    docs with `cargo doc --open` to see if things looked mostly the same
    with default features.

--- src/utils/const_fns.rs
+++ src/utils/const_fns.rs
@@ -5,11 +5,9 @@ use crate::timezone::{LeapSecond, Transition};
 
 use std::cmp::Ordering;
 
-use const_fn::const_fn;
-
 /// Compare two values
-#[const_fn(feature = "const")]
-pub const fn cmp(a: i64, b: i64) -> Ordering {
+#[cfg_attr(feature = "const", const_fn::const_fn)]
+pub fn cmp(a: i64, b: i64) -> Ordering {
     if a < b {
         Ordering::Less
     } else if a == b {
@@ -20,8 +18,8 @@ pub const fn cmp(a: i64, b: i64) -> Ordering {
 }
 
 /// Returns the minimum of two values
-#[const_fn(feature = "const")]
-pub const fn min(a: i64, b: i64) -> i64 {
+#[cfg_attr(feature = "const", const_fn::const_fn)]
+pub fn min(a: i64, b: i64) -> i64 {
     match cmp(a, b) {
         Ordering::Less | Ordering::Equal => a,
         Ordering::Greater => b,
@@ -43,14 +41,14 @@ macro_rules! impl_try_into_integer {
 }
 
 /// Convert a `i64` value to a `i32` value
-#[const_fn(feature = "const")]
-pub const fn try_into_i32(value: i64) -> Result<i32, OutOfRangeError> {
+#[cfg_attr(feature = "const", const_fn::const_fn)]
+pub fn try_into_i32(value: i64) -> Result<i32, OutOfRangeError> {
     impl_try_into_integer!(i64, i32, value)
 }
 
 /// Convert a `i128` value to a `i64` value
-#[const_fn(feature = "const")]
-pub const fn try_into_i64(value: i128) -> Result<i64, OutOfRangeError> {
+#[cfg_attr(feature = "const", const_fn::const_fn)]
+pub fn try_into_i64(value: i128) -> Result<i64, OutOfRangeError> {
     impl_try_into_integer!(i128, i64, value)
 }
 
@@ -79,25 +77,25 @@ macro_rules! impl_binary_search {
 }
 
 /// Copy the input value
-#[const_fn(feature = "const")]
-const fn copied(x: &i64) -> i64 {
+#[cfg_attr(feature = "const", const_fn::const_fn)]
+fn copied(x: &i64) -> i64 {
     *x
 }
 
 /// Binary searches a sorted `i64` slice for the given element
-#[const_fn(feature = "const")]
-pub const fn binary_search_i64(slice: &[i64], x: i64) -> Result<usize, usize> {
+#[cfg_attr(feature = "const", const_fn::const_fn)]
+pub fn binary_search_i64(slice: &[i64], x: i64) -> Result<usize, usize> {
     impl_binary_search!(slice, copied, x)
 }
 
 /// Binary searches a sorted `Transition` slice for the given element
-#[const_fn(feature = "const")]
-pub const fn binary_search_transitions(slice: &[Transition], x: i64) -> Result<usize, usize> {
+#[cfg_attr(feature = "const", const_fn::const_fn)]
+pub fn binary_search_transitions(slice: &[Transition], x: i64) -> Result<usize, usize> {
     impl_binary_search!(slice, Transition::unix_leap_time, x)
 }
 
 /// Binary searches a sorted `LeapSecond` slice for the given element
-#[const_fn(feature = "const")]
-pub const fn binary_search_leap_seconds(slice: &[LeapSecond], x: i64) -> Result<usize, usize> {
+#[cfg_attr(feature = "const", const_fn::const_fn)]
+pub fn binary_search_leap_seconds(slice: &[LeapSecond], x: i64) -> Result<usize, usize> {
     impl_binary_search!(slice, LeapSecond::unix_leap_time, x)
 }
